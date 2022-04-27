c33ea65d4ff93d25c7161b669b485c3087868858
Abdulaziz <mukhamedovabdulaziz@gmail.com>
2022-04-27 23:29:11 +0600

    update files

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQHeTQsBATkdwg/company-logo_200_200/0/1575544858313?e=2147483647&v=beta&t=18ncVsO6jYPAgpdianbDyvnJdSZsPfs38h2xMseE9eo" alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                Main Content
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ social-network-ts/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                APP HELLO
+                {/*<Route path="/hello" render={() => <HelloMessage message={"Hello friends"}/>}/>*/}
+                {/*<Route path="/bye" render={<ByeMessage message={"Bye samurai"}/>}/>*/}
+            </div>
+        </BrowserRouter>
+    );
+}
+
+type MessageType = {
+    message: string
+}
+
+function HelloMessage(props: MessageType) {
+    return <h1>{props.message}</h1>
+}
+
+const ByeMessage: React.FC<MessageType> = (props) => {
+    return <h1>{props.message}</h1>
+}
+
+export default App;
