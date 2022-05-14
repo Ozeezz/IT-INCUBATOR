41fcb8e7253240669f33249c9756d1386b64241a
Abdulaziz <mukhamedovabdulaziz@gmail.com>
2022-05-14 15:25:31 +0600

    test

---
+++ samurai-way-main/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQHeTQsBATkdwg/company-logo_200_200/0/1575544858313?e=2147483647&v=beta&t=18ncVsO6jYPAgpdianbDyvnJdSZsPfs38h2xMseE9eo"
+                alt="logo" width="200"/>
+        </header>
+    )
+}
